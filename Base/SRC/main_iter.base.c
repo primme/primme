primme/primme

commit 68c4dd11f36660fe513f38fdd890bc71e487eb85

Instead of doing RR with candidates with too small singular values (in prepare_vecs), it is recomputed QR.

Base/SRC/main_iter.base.c
@@ -430,8 +430,27 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
 
             availableBlockSize = min(availableBlockSize, maxRecentlyConverged+1);
 
+            /* If QR decomposition accumulates so much error, force it to     */
+            /* reset by setting targetShiftIndex to -1. We use the next       */
+            /* heuristic. Note that if (s_0, u_0, y_0) is the triplet of R    */
+            /* with the smallest singular value, (A-tau*I)*V = Q*R, and       */
+            /* l_0 is the Rayleigh quotient of V*y_0, then                    */
+            /*    |l_0-tau| = |y_0'*V'*(A-tau*I)*V*y_0| = |y_0'*V'*Q*R*y_0| = */
+            /*       = |y_0'*V'*Q*u_0*s_0| <= s_0.                            */
+            /* So when |l_0-tau|-machEps*|A| > s_0, we consider to reset the  */
+            /* QR factorization. machEps*|A| is the error computing l_0.      */
+
+            if (primme->projectionParams.projection == primme_proj_refined &&
+                  fabs(primme->targetShifts[targetShiftIndex]-hVals[0])
+                  -primme->stats.estimateLargestSVal*machEps > hSVals[0]) {
+
+               availableBlockSize = 0;
+               targetShiftIndex = -1;
+            } 
+
             /* Set the block with the first unconverged pairs */
-            prepare_candidates_@(pre)primme(V, W, primme->nLocal, H,
+            if (availableBlockSize > 0) {
+               prepare_candidates_@(pre)primme(V, W, primme->nLocal, H,
                   primme->maxBasisSize, basisSize, primme->nLocal,
                   &V[basisSize*primme->nLocal], &W[basisSize*primme->nLocal],
                   hVecs, basisSize, hVals, hSVals, flags,
@@ -440,6 +459,10 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
                   resNorms, targetShiftIndex, machEps, iev, &blockSize,
                   &recentlyConverged, &numArbitraryVecs, &smallestResNorm,
                   rwork, rworkSize, iwork, primme);
+            }
+            else {
+               blockSize = recentlyConverged = 0;
+            }
 
             /* print residuals */
             print_residuals(hVals, blockNorms, numConverged, numLocked, iev, blockSize,
@@ -457,12 +480,15 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
 
             if (numConverged >= primme->numEvals ||
                 (primme->locking && recentlyConverged > 0
-                    && primme->target != primme_smallest
-                    && primme->target != primme_largest) ||
-                (Q && min(primme->numTargetShifts, numConverged) !=
-                        min(primme->numTargetShifts, numConverged-recentlyConverged))
-                              ) {
+                  && primme->target != primme_smallest
+                  && primme->target != primme_largest) ||
+                targetShiftIndex < 0 ||
+                (Q && primme->targetShifts[targetShiftIndex] !=
+                  primme->targetShifts[
+                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+
                break;
+
             }
 
             /* If the block size is zero, the whole basis spans an exact     */
@@ -597,7 +623,8 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
          /* converged.                                                        */
          /* ----------------------------------------------------------------- */
 
-         prepare_vecs_@(pre)primme(basisSize, 0, availableBlockSize, H,
+         if (targetShiftIndex > 0)
+            prepare_vecs_@(pre)primme(basisSize, 0, availableBlockSize, H,
                primme->maxBasisSize, hVals, hSVals, hVecs, basisSize,
                targetShiftIndex, &numArbitraryVecs, smallestResNorm, NULL, 1,
                machEps, rworkSize, rwork, iwork, primme);

PRIMMESRC/DSRC/main_iter_d.c
@@ -425,8 +425,27 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
 
             availableBlockSize = min(availableBlockSize, maxRecentlyConverged+1);
 
+            /* If QR decomposition accumulates so much error, force it to     */
+            /* reset by setting targetShiftIndex to -1. We use the next       */
+            /* heuristic. Note that if (s_0, u_0, y_0) is the triplet of R    */
+            /* with the smallest singular value, (A-tau*I)*V = Q*R, and       */
+            /* l_0 is the Rayleigh quotient of V*y_0, then                    */
+            /*    |l_0-tau| = |y_0'*V'*(A-tau*I)*V*y_0| = |y_0'*V'*Q*R*y_0| = */
+            /*       = |y_0'*V'*Q*u_0*s_0| <= s_0.                            */
+            /* So when |l_0-tau|-machEps*|A| > s_0, we consider to reset the  */
+            /* QR factorization. machEps*|A| is the error computing l_0.      */
+
+            if (primme->projectionParams.projection == primme_proj_refined &&
+                  fabs(primme->targetShifts[targetShiftIndex]-hVals[0])
+                  -primme->stats.estimateLargestSVal*machEps > hSVals[0]) {
+
+               availableBlockSize = 0;
+               targetShiftIndex = -1;
+            } 
+
             /* Set the block with the first unconverged pairs */
-            prepare_candidates_dprimme(V, W, primme->nLocal, H,
+            if (availableBlockSize > 0) {
+               prepare_candidates_dprimme(V, W, primme->nLocal, H,
                   primme->maxBasisSize, basisSize, primme->nLocal,
                   &V[basisSize*primme->nLocal], &W[basisSize*primme->nLocal],
                   hVecs, basisSize, hVals, hSVals, flags,
@@ -435,6 +454,10 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
                   resNorms, targetShiftIndex, machEps, iev, &blockSize,
                   &recentlyConverged, &numArbitraryVecs, &smallestResNorm,
                   rwork, rworkSize, iwork, primme);
+            }
+            else {
+               blockSize = recentlyConverged = 0;
+            }
 
             /* print residuals */
             print_residuals(hVals, blockNorms, numConverged, numLocked, iev, blockSize,
@@ -452,12 +475,15 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
 
             if (numConverged >= primme->numEvals ||
                 (primme->locking && recentlyConverged > 0
-                    && primme->target != primme_smallest
-                    && primme->target != primme_largest) ||
-                (Q && min(primme->numTargetShifts, numConverged) !=
-                        min(primme->numTargetShifts, numConverged-recentlyConverged))
-                              ) {
+                  && primme->target != primme_smallest
+                  && primme->target != primme_largest) ||
+                targetShiftIndex < 0 ||
+                (Q && primme->targetShifts[targetShiftIndex] !=
+                  primme->targetShifts[
+                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+
                break;
+
             }
 
             /* If the block size is zero, the whole basis spans an exact     */
@@ -592,7 +618,8 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
          /* converged.                                                        */
          /* ----------------------------------------------------------------- */
 
-         prepare_vecs_dprimme(basisSize, 0, availableBlockSize, H,
+         if (targetShiftIndex > 0)
+            prepare_vecs_dprimme(basisSize, 0, availableBlockSize, H,
                primme->maxBasisSize, hVals, hSVals, hVecs, basisSize,
                targetShiftIndex, &numArbitraryVecs, smallestResNorm, NULL, 1,
                machEps, rworkSize, rwork, iwork, primme);

PRIMMESRC/ZSRC/main_iter_z.c
@@ -425,8 +425,27 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
 
             availableBlockSize = min(availableBlockSize, maxRecentlyConverged+1);
 
+            /* If QR decomposition accumulates so much error, force it to     */
+            /* reset by setting targetShiftIndex to -1. We use the next       */
+            /* heuristic. Note that if (s_0, u_0, y_0) is the triplet of R    */
+            /* with the smallest singular value, (A-tau*I)*V = Q*R, and       */
+            /* l_0 is the Rayleigh quotient of V*y_0, then                    */
+            /*    |l_0-tau| = |y_0'*V'*(A-tau*I)*V*y_0| = |y_0'*V'*Q*R*y_0| = */
+            /*       = |y_0'*V'*Q*u_0*s_0| <= s_0.                            */
+            /* So when |l_0-tau|-machEps*|A| > s_0, we consider to reset the  */
+            /* QR factorization. machEps*|A| is the error computing l_0.      */
+
+            if (primme->projectionParams.projection == primme_proj_refined &&
+                  fabs(primme->targetShifts[targetShiftIndex]-hVals[0])
+                  -primme->stats.estimateLargestSVal*machEps > hSVals[0]) {
+
+               availableBlockSize = 0;
+               targetShiftIndex = -1;
+            } 
+
             /* Set the block with the first unconverged pairs */
-            prepare_candidates_zprimme(V, W, primme->nLocal, H,
+            if (availableBlockSize > 0) {
+               prepare_candidates_zprimme(V, W, primme->nLocal, H,
                   primme->maxBasisSize, basisSize, primme->nLocal,
                   &V[basisSize*primme->nLocal], &W[basisSize*primme->nLocal],
                   hVecs, basisSize, hVals, hSVals, flags,
@@ -435,6 +454,10 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
                   resNorms, targetShiftIndex, machEps, iev, &blockSize,
                   &recentlyConverged, &numArbitraryVecs, &smallestResNorm,
                   rwork, rworkSize, iwork, primme);
+            }
+            else {
+               blockSize = recentlyConverged = 0;
+            }
 
             /* print residuals */
             print_residuals(hVals, blockNorms, numConverged, numLocked, iev, blockSize,
@@ -452,12 +475,15 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
 
             if (numConverged >= primme->numEvals ||
                 (primme->locking && recentlyConverged > 0
-                    && primme->target != primme_smallest
-                    && primme->target != primme_largest) ||
-                (Q && min(primme->numTargetShifts, numConverged) !=
-                        min(primme->numTargetShifts, numConverged-recentlyConverged))
-                              ) {
+                  && primme->target != primme_smallest
+                  && primme->target != primme_largest) ||
+                targetShiftIndex < 0 ||
+                (Q && primme->targetShifts[targetShiftIndex] !=
+                  primme->targetShifts[
+                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+
                break;
+
             }
 
             /* If the block size is zero, the whole basis spans an exact     */
@@ -592,7 +618,8 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
          /* converged.                                                        */
          /* ----------------------------------------------------------------- */
 
-         prepare_vecs_zprimme(basisSize, 0, availableBlockSize, H,
+         if (targetShiftIndex > 0)
+            prepare_vecs_zprimme(basisSize, 0, availableBlockSize, H,
                primme->maxBasisSize, hVals, hSVals, hVecs, basisSize,
                targetShiftIndex, &numArbitraryVecs, smallestResNorm, NULL, 1,
                machEps, rworkSize, rwork, iwork, primme);

Base/SRC/solve_H.base.c
@@ -870,7 +870,6 @@ int prepare_vecs_@(pre)primme(int basisSize, int i0, int blockSize,
          /* sin current and previous hVecs(:,i): hVecs(end,i)                 */
          /* NOTE: we don't want to check hVecs(end,i) just after restart, so  */
          /* we don't use the value when it is zero.                           */
-         /* Also check that singular value is larger than the residual norm   */
 
          double ip0 = fabs(*(double*)&hVecs[(i-1)*ldhVecs+basisSize-1]);
          double ip = (flags && ip0 != 0.0) ? ip0 : HUGE_VAL;
@@ -879,9 +878,7 @@ int prepare_vecs_@(pre)primme(int basisSize, int i0, int blockSize,
 
          if (!flags || flags[i-1] == UNCONVERGED) someCandidate = 1;
 
-         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff 
-               && fabs(hVals[i-1]-targetShift) < hSVals[i-1]+machEps*hSVals[basisSize-1]
-               && hSVals[i-1] >= smallestResNorm)
+         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff) 
             break;
       }
       i = min(i, basisSize);
@@ -954,7 +951,6 @@ int prepare_vecs_@(pre)primme(int basisSize, int i0, int blockSize,
    }
 
    permute_vecs_dprimme(hVals, 1, i, 1, perm, (double*)rwork, iwork);
-   permute_vecs_dprimme(hSVals, 1, i, 1, perm, (double*)rwork, iwork);
    permute_vecs_@(pre)primme(hVecs, basisSize, i, ldhVecs, perm, rwork, iwork);
 
    /* If something has changed between arbitraryVecs and i, notify */

PRIMMESRC/DSRC/solve_H_d.c
@@ -744,7 +744,6 @@ int prepare_vecs_dprimme(int basisSize, int i0, int blockSize,
          /* sin current and previous hVecs(:,i): hVecs(end,i)                 */
          /* NOTE: we don't want to check hVecs(end,i) just after restart, so  */
          /* we don't use the value when it is zero.                           */
-         /* Also check that singular value is larger than the residual norm   */
 
          double ip0 = fabs(*(double*)&hVecs[(i-1)*ldhVecs+basisSize-1]);
          double ip = (flags && ip0 != 0.0) ? ip0 : HUGE_VAL;
@@ -753,9 +752,7 @@ int prepare_vecs_dprimme(int basisSize, int i0, int blockSize,
 
          if (!flags || flags[i-1] == UNCONVERGED) someCandidate = 1;
 
-         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff 
-               && fabs(hVals[i-1]-targetShift) < hSVals[i-1]+machEps*hSVals[basisSize-1]
-               && hSVals[i-1] >= smallestResNorm)
+         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff) 
             break;
       }
       i = min(i, basisSize);
@@ -828,7 +825,6 @@ int prepare_vecs_dprimme(int basisSize, int i0, int blockSize,
    }
 
    permute_vecs_dprimme(hVals, 1, i, 1, perm, (double*)rwork, iwork);
-   permute_vecs_dprimme(hSVals, 1, i, 1, perm, (double*)rwork, iwork);
    permute_vecs_dprimme(hVecs, basisSize, i, ldhVecs, perm, rwork, iwork);
 
    /* If something has changed between arbitraryVecs and i, notify */

PRIMMESRC/ZSRC/solve_H_z.c
@@ -762,7 +762,6 @@ int prepare_vecs_zprimme(int basisSize, int i0, int blockSize,
          /* sin current and previous hVecs(:,i): hVecs(end,i)                 */
          /* NOTE: we don't want to check hVecs(end,i) just after restart, so  */
          /* we don't use the value when it is zero.                           */
-         /* Also check that singular value is larger than the residual norm   */
 
          double ip0 = fabs(*(double*)&hVecs[(i-1)*ldhVecs+basisSize-1]);
          double ip = (flags && ip0 != 0.0) ? ip0 : HUGE_VAL;
@@ -771,9 +770,7 @@ int prepare_vecs_zprimme(int basisSize, int i0, int blockSize,
 
          if (!flags || flags[i-1] == UNCONVERGED) someCandidate = 1;
 
-         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff 
-               && fabs(hVals[i-1]-targetShift) < hSVals[i-1]+machEps*hSVals[basisSize-1]
-               && hSVals[i-1] >= smallestResNorm)
+         if (fabs(hSVals[i]-hSVals[i-1]) >= minDiff) 
             break;
       }
       i = min(i, basisSize);
@@ -846,7 +843,6 @@ int prepare_vecs_zprimme(int basisSize, int i0, int blockSize,
    }
 
    permute_vecs_dprimme(hVals, 1, i, 1, perm, (double*)rwork, iwork);
-   permute_vecs_dprimme(hSVals, 1, i, 1, perm, (double*)rwork, iwork);
    permute_vecs_zprimme(hVecs, basisSize, i, ldhVecs, perm, rwork, iwork);
 
    /* If something has changed between arbitraryVecs and i, notify */

commit 9ac7f8e9787469393d9ab17862d7c0c839f140e6

Fixed bug introduced in commit 68c4dd1.

Base/SRC/main_iter.base.c
@@ -447,6 +447,7 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
 
                   availableBlockSize = 0;
                   targetShiftIndex = -1;
+                  break;
                } 
             }
 
@@ -487,7 +488,7 @@ int main_iter_@(pre)primme(double *evals, int *perm, @(type) *evecs,
                 targetShiftIndex < 0 ||
                 (Q && primme->targetShifts[targetShiftIndex] !=
                   primme->targetShifts[
-                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+                     min(primme->numTargetShifts-1, numConverged)])) {
 
                break;
 

PRIMMESRC/DSRC/main_iter_d.c
@@ -442,6 +442,7 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
 
                   availableBlockSize = 0;
                   targetShiftIndex = -1;
+                  break;
                } 
             }
 
@@ -482,7 +483,7 @@ int main_iter_dprimme(double *evals, int *perm, double *evecs,
                 targetShiftIndex < 0 ||
                 (Q && primme->targetShifts[targetShiftIndex] !=
                   primme->targetShifts[
-                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+                     min(primme->numTargetShifts-1, numConverged)])) {
 
                break;
 

PRIMMESRC/ZSRC/main_iter_z.c
@@ -442,6 +442,7 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
 
                   availableBlockSize = 0;
                   targetShiftIndex = -1;
+                  break;
                } 
             }
 
@@ -482,7 +483,7 @@ int main_iter_zprimme(double *evals, int *perm, Complex_Z *evecs,
                 targetShiftIndex < 0 ||
                 (Q && primme->targetShifts[targetShiftIndex] !=
                   primme->targetShifts[
-                     min(primme->numTargetShifts-1, max(0,numConverged-1))])) {
+                     min(primme->numTargetShifts-1, numConverged)])) {
 
                break;
 