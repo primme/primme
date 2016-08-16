9c39cbb778cac2749e713cc031ade16b03a2fdad
Eloy Romero <eloy@cs.wm.edu>
2016-08-16 14:28:52 -0400

    Fixed tests report to be useful also for SVD.

--- TEST/driver.c
+++ TEST/driver.c
@@ -332,13 +332,13 @@ static int real_main (int argc, char *argv[]) {
       }
       fprintf(primme.outputFile, " %d eigenpairs converged\n", primme.initSize);
 
-      fprintf(primme.outputFile, "Tolerance : %-22.15E\n", 
+      fprintf(primme.outputFile, "Tolerance  : %-22.15E\n", 
                                                             primme.aNorm*primme.eps);
-      fprintf(primme.outputFile, "Iterations: %-d\n", 
+      fprintf(primme.outputFile, "Iterations : %-d\n", 
                                                     primme.stats.numOuterIterations); 
-      fprintf(primme.outputFile, "Restarts  : %-d\n", primme.stats.numRestarts);
-      fprintf(primme.outputFile, "Matvecs   : %-d\n", primme.stats.numMatvecs);
-      fprintf(primme.outputFile, "Preconds  : %-d\n", primme.stats.numPreconds);
+      fprintf(primme.outputFile, "Restarts   : %-d\n", primme.stats.numRestarts);
+      fprintf(primme.outputFile, "Matvecs    : %-d\n", primme.stats.numMatvecs);
+      fprintf(primme.outputFile, "Preconds   : %-d\n", primme.stats.numPreconds);
       if (primme.locking && primme.intWork && primme.intWork[0] == 1) {
          fprintf(primme.outputFile, "\nA locking problem has occurred.\n");
          fprintf(primme.outputFile,
