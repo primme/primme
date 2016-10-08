fc6e1f6e2038a2bc0fc331a0470b1ff738d04fa6
Eloy Romero <eloy@cs.wm.edu>
2016-10-08 16:50:05 -0400

    Added to setup.py extra linking arguments for MacOS.

--- PYTHON/setup.py
+++ PYTHON/setup.py
@@ -11,12 +11,17 @@ try:
 except:
     from numpy.__config__ import get_info
 
-# Obtain the numpy include directory.
+# Obtain the numpy include directory
 numpy_include = numpy.get_include()
+
+# Obtain BLAS/LAPACK linking options
 lapack_info = get_info('lapack_opt')
 blas_info = get_info('blas_opt')
-blaslapack_libraries = lapack_info.get('libraries', ['lapack']) + blas_info.get('libraries', ['blas'])
+blaslapack_libraries = lapack_info.get('libraries', []) + blas_info.get('libraries', [])
 blaslapack_library_dirs = lapack_info.get('library_dirs', []) + blas_info.get('library_dirs', [])
+blaslapack_extra_link_args = lapack_info.get('extra_link_args', []) + blas_info.get('extra_link_args', [])
+if not blaslapack_libraries and not blaslapack_extra_link_args:
+    blaslapack_libraries = ['lapack', 'blas']
 
 # Array extension module
 _Primme = Extension("_Primme",
@@ -24,6 +29,7 @@ _Primme = Extension("_Primme",
                    include_dirs = [numpy_include],
                    library_dirs = ["../lib"] + blaslapack_library_dirs,
                    libraries = ["primme"] + blaslapack_libraries,
+                   extra_link_args = blaslapack_extra_link_args
                    #extra_compile_args = ["-g", "-O0", "-Wall", "-Wextra"]
                    )
 
