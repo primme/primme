primme/primme

commit 8bf6e8f69a987e9391da578d917d48bd17372956

Only do right projection when using preconditioning. Restart H explicitly as hVecs'*H*hVecs when normal operator. Extended compute_submatrix for non-Hermitian matrices.

src/eigs/correction.c
@@ -339,7 +339,10 @@ int solve_correction_Sprimme(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
           Olsen_preconditioner_block(r, ldW, x, ldV, Bx, ldBV, blockSize, ctx);
       }
       else {
-         if ( primme->correctionParams.projectors.RightX ) {   
+         if (primme->correctionParams.projectors.RightX &&
+               ((primme->correctionParams.precondition &&
+                      primme->applyPreconditioner) ||
+                     Bx != x)) {
 #ifdef USE_HERMITIAN
             /*Compute a cheap approximation to OLSENS, where (x'Kinvr)/xKinvx */
             /*is approximated by e: Kinvr-e*KinvBx=Kinv(r-e*x)=Kinv(I-ct*x*x')r*/
@@ -350,7 +353,7 @@ int solve_correction_Sprimme(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
                      &Bx[ldBV * blockIndex], 1, &r[ldW * blockIndex], 1, ctx);
             }
 #else
-            return PRIMME_FUNCTION_UNAVAILABLE;
+            CHKERR(PRIMME_FUNCTION_UNAVAILABLE);
 #endif
          }
 
@@ -443,7 +446,7 @@ int solve_correction_Sprimme(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
       CHKERR(Num_free_Sprimme(KinvBx, ctx));
       CHKERR(KIND(Num_free_RHprimme, Num_free_SHprimme)(blockRitzVals, ctx));
 #else
-   return  PRIMME_FUNCTION_UNAVAILABLE;
+      CHKERR(PRIMME_FUNCTION_UNAVAILABLE);
 #endif /* USE_HERMITIAN */
    } /* JDqmr variants */
 

src/eigs/restart.c
@@ -1244,7 +1244,7 @@ int Num_aux_update_VWXR_Sprimme(SCALAR *V, SCALAR *W, SCALAR *BV,
             Bevecs?&Bevecs[ldBevecs*BevecsSize]:NULL, nBX2b, nBX2e, ldBevecs,
             rnorms, nrb, nre,
             VtBV?&VtBV[evecsSize*ldVtBV+evecsSize]:NULL, nVtBV, ldVtBV,
-            primme->orth == primme_orth_explicit_I?H:NULL, nH, ldH,
+            KIND(primme->orth == primme_orth_explicit_I,1)?H:NULL, nH, ldH,
             NULL, 0, 0,
             ctx));
 
@@ -1600,16 +1600,15 @@ STATIC int restart_RR(HSCALAR *H, int ldH, HSCALAR *VtBV, int ldVtBV,
    int orderedIndexOfPreviousVecs;  /* index of prev. vecs after applying hVecsPerm */
    double aNorm = primme?max(primme->aNorm, primme->stats.estimateLargestSVal):0.0;
 
+#ifdef USE_HERMITIAN
    if (primme->orth == primme_orth_implicit_I) {
-   /* ---------------------------------------------------------------------- */
-   /* If coefficient vectors from the previous iteration were retained, then */
-   /* insert the computed overlap matrix into the restarted H                */
-   /* ---------------------------------------------------------------------- */
 
-      CHKERR(compute_submatrix_SHprimme(&hVecs[ldhVecs*indexOfPreviousVecs],
-               numPrevRetained, ldhVecs, H,
-               basisSize, ldH, &H[ldH*indexOfPreviousVecs+indexOfPreviousVecs],
-               ldH, ctx));
+      // If coefficient vectors from the previous iteration were retained, then
+      // insert the computed overlap matrix into the restarted H
+
+      CHKERR(compute_submatrix_SHprimme(&hVecs[ldhVecs * indexOfPreviousVecs],
+            numPrevRetained, ldhVecs, H, basisSize, ldH, 1 /* Hermitian */,
+            &H[ldH * indexOfPreviousVecs + indexOfPreviousVecs], ldH, ctx));
 
       /* ----------------------------------------------------------------- */
       /* Y = V*hVecs([0:indexOfPreviousVecs-1 \                            */
@@ -1619,24 +1618,22 @@ STATIC int restart_RR(HSCALAR *H, int ldH, HSCALAR *VtBV, int ldVtBV,
       /* part of H and VtBV.                                               */
       /* ----------------------------------------------------------------- */
 
-      for (j=0; j < indexOfPreviousVecs; j++) {
-         for (i=0; i < j; i++) {
-            H[ldH*j+i] = 0.0;
-         }
-         H[ldH*j+j] = hVals[j];
+      CHKERR(Num_zero_matrix_Sprimme(
+            H, restartSize, indexOfPreviousVecs, ldH, ctx));
+      CHKERR(Num_zero_matrix_Sprimme(&H[ldH * indexOfPreviousVecs],
+            indexOfPreviousVecs, numPrevRetained, ldH, ctx));
+      CHKERR(Num_zero_matrix_Sprimme(
+            &H[ldH * (indexOfPreviousVecs + numPrevRetained)], restartSize,
+            restartSize - indexOfPreviousVecs - numPrevRetained, ldH, ctx));
+
+      for (j = 0; j < indexOfPreviousVecs; j++) {
+         H[ldH * j + j] = hVals[j];
       }
-      for (j=indexOfPreviousVecs; j<indexOfPreviousVecs+numPrevRetained; j++) {
-         for (i=0; i < indexOfPreviousVecs; i++) {
-            H[ldH*j+i] = 0.0;
-         }
-      }
-      for (j=indexOfPreviousVecs+numPrevRetained; j < restartSize; j++) {
-         for (i=0; i < j; i++) {
-            H[ldH*j+i] = 0.0;
-         }
-         H[ldH*j+j] = hVals[j];
+      for (j = indexOfPreviousVecs + numPrevRetained; j < restartSize; j++) {
+         H[ldH * j + j] = hVals[j];
       }
    }
+#endif /* USE_HERMITIAN */
 
    /* ---------------------------------------------------------------------- */
    /* Solve the whole matrix when the targetShift has to change              */ 
@@ -1829,10 +1826,12 @@ STATIC int restart_refined(SCALAR *V, PRIMME_INT ldV, SCALAR *W, PRIMME_INT ldW,
    /* Replace H by hVecs' * H * hVecs */
    /* ------------------------------- */
 
+#ifdef USE_HERMITIAN
    if (H && primme->orth == primme_orth_implicit_I) {
-      CHKERR(compute_submatrix_SHprimme(
-            hVecs, restartSize, ldhVecs, H, basisSize, ldH, H, ldH, ctx));
+      CHKERR(compute_submatrix_SHprimme(hVecs, restartSize, ldhVecs, H,
+            basisSize, ldH, 1 /* Hermitian */, H, ldH, ctx));
    }
+#endif /* USE_HERMITIAN */
 
    /* -------------------------------------- */
    /* Quick exit if the target has changed   */
@@ -2241,10 +2240,12 @@ STATIC int restart_harmonic(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
    /* Replace H by hVecs' * H * hVecs */
    /* ------------------------------- */
 
+#ifdef USE_HERMITIAN
    if (primme->orth == primme_orth_implicit_I) {
-      CHKERR(compute_submatrix_SHprimme(
-               hVecs, restartSize, ldhVecs, H, basisSize, ldH, H, ldH, ctx));
+      CHKERR(compute_submatrix_SHprimme(hVecs, restartSize, ldhVecs, H,
+            basisSize, ldH, 1 /* Hermitian */, H, ldH, ctx));
    }
+#endif /* USE_HERMITIAN */
 
    /* ------------------------------- */
    /* Update targetShiftIndex         */

src/eigs/solve_projection.c
@@ -956,8 +956,10 @@ int prepare_vecs_Sprimme(int basisSize, int i0, int blockSize, SCALAR *H,
             hVecsRot[ldhVecsRot*k+k] = 1.0;
  
          /* aH = hVecs(:,j:i-1)'*H*hVecs(:,j:i-1) */
-         CHKERR(compute_submatrix_Sprimme(&hVecs[ldhVecs*j], aBasisSize,
-               ldhVecs, H, basisSize, ldH, aH, aBasisSize, ctx));
+         CHKERR(compute_submatrix_Sprimme(&hVecs[ldhVecs * j], aBasisSize,
+               ldhVecs, H, basisSize, ldH,
+               KIND(1 /* Hermitian */, 0 /* non-Hermitian */), aH, aBasisSize,
+               ctx));
 
          /* Compute and sort eigendecomposition aH*ahVecs = ahVecs*diag(hVals(j:i-1)) */
          CHKERR(solve_H_RR_Sprimme(aH, aBasisSize, NULL, 0, ahVecs, ldhVecsRot,

src/include/auxiliary.h
@@ -770,7 +770,7 @@ int Num_copy_compact_trimatrix_dprimme(dummy_type_dprimme *x, PRIMME_INT m, int 
 #  define compute_submatrix_RHqprimme CONCAT(compute_submatrix_,CONCAT(CONCAT(CONCAT(,q),primme),))
 #endif
 int compute_submatrix_dprimme(dummy_type_dprimme *X, int nX, int ldX, dummy_type_dprimme *H, int nH,
-                              int ldH, dummy_type_dprimme *R, int ldR, primme_context ctx);
+      int ldH, int isherm, dummy_type_dprimme *R, int ldR, primme_context ctx);
 #if !defined(CHECK_TEMPLATE) && !defined(Num_copy_matrix_columns_Sprimme)
 #  define Num_copy_matrix_columns_Sprimme CONCAT(Num_copy_matrix_columns_,SCALAR_SUF)
 #endif
@@ -1383,7 +1383,7 @@ int Num_copy_trimatrix_compact_sprimme(dummy_type_sprimme *x, PRIMME_INT m, int 
 int Num_copy_compact_trimatrix_sprimme(dummy_type_sprimme *x, PRIMME_INT m, int n, int i0,
       dummy_type_sprimme *y, int ldy);
 int compute_submatrix_sprimme(dummy_type_sprimme *X, int nX, int ldX, dummy_type_sprimme *H, int nH,
-                              int ldH, dummy_type_sprimme *R, int ldR, primme_context ctx);
+      int ldH, int isherm, dummy_type_sprimme *R, int ldR, primme_context ctx);
 int Num_copy_matrix_columns_sprimme(dummy_type_sprimme *x, PRIMME_INT m, int *xin, int n,
                                      PRIMME_INT ldx, dummy_type_sprimme *y, int *yin,
                                      PRIMME_INT ldy, primme_context ctx);
@@ -1409,7 +1409,7 @@ int Num_copy_trimatrix_compact_cprimme(dummy_type_cprimme *x, PRIMME_INT m, int 
 int Num_copy_compact_trimatrix_cprimme(dummy_type_cprimme *x, PRIMME_INT m, int n, int i0,
       dummy_type_cprimme *y, int ldy);
 int compute_submatrix_cprimme(dummy_type_cprimme *X, int nX, int ldX, dummy_type_cprimme *H, int nH,
-                              int ldH, dummy_type_cprimme *R, int ldR, primme_context ctx);
+      int ldH, int isherm, dummy_type_cprimme *R, int ldR, primme_context ctx);
 int Num_copy_matrix_columns_cprimme(dummy_type_cprimme *x, PRIMME_INT m, int *xin, int n,
                                      PRIMME_INT ldx, dummy_type_cprimme *y, int *yin,
                                      PRIMME_INT ldy, primme_context ctx);
@@ -1435,7 +1435,7 @@ int Num_copy_trimatrix_compact_zprimme(dummy_type_zprimme *x, PRIMME_INT m, int 
 int Num_copy_compact_trimatrix_zprimme(dummy_type_zprimme *x, PRIMME_INT m, int n, int i0,
       dummy_type_zprimme *y, int ldy);
 int compute_submatrix_zprimme(dummy_type_zprimme *X, int nX, int ldX, dummy_type_zprimme *H, int nH,
-                              int ldH, dummy_type_zprimme *R, int ldR, primme_context ctx);
+      int ldH, int isherm, dummy_type_zprimme *R, int ldR, primme_context ctx);
 int Num_copy_matrix_columns_zprimme(dummy_type_zprimme *x, PRIMME_INT m, int *xin, int n,
                                      PRIMME_INT ldx, dummy_type_zprimme *y, int *yin,
                                      PRIMME_INT ldy, primme_context ctx);

src/linalg/auxiliary.c
@@ -413,7 +413,8 @@ int Num_copy_compact_trimatrix_Sprimme(SCALAR *x, PRIMME_INT m, int n, int i0,
 
 /*******************************************************************************
  * Subroutine compute_submatrix - This subroutine computes the nX x nX submatrix
- *    R = X'*H*X, where H stores the upper triangular part of a symmetric matrix.
+ *    R = X'*H*X, where H stores the upper triangular part of a symmetric matrix,
+ *    or H is a full non-Hermitian matrix.
  *    
  * Input parameters
  * ----------------
@@ -427,9 +428,7 @@ int Num_copy_compact_trimatrix_Sprimme(SCALAR *x, PRIMME_INT m, int n, int i0,
  *
  * ldH      Leading dimension of H
  *
- * rwork    Work array.  Must be of size nH x nX
- *
- * lrwork   Length of the work array
+ * isherm   whether H is Hermitian
  *
  * ldR      Leading dimension of R
  *
@@ -442,16 +441,26 @@ int Num_copy_compact_trimatrix_Sprimme(SCALAR *x, PRIMME_INT m, int n, int i0,
 #if !defined(USE_HALF) && !defined(USE_HALFCOMPLEX)
 TEMPLATE_PLEASE
 int compute_submatrix_Sprimme(SCALAR *X, int nX, int ldX, SCALAR *H, int nH,
-                              int ldH, SCALAR *R, int ldR, primme_context ctx) {
+      int ldH, int isherm, SCALAR *R, int ldR, primme_context ctx) {
 
-  if (nH == 0 || nX == 0)
-    return 0;
+   if (nH == 0 || nX == 0) return 0;
 
    SCALAR *rwork;
    CHKERR(Num_malloc_Sprimme((size_t)nH * (size_t)nX, &rwork, ctx));
+
+   /* rwork = H * X */
+
    Num_zero_matrix_Sprimme(rwork, nH, nX, nH, ctx);
-   CHKERR(Num_hemm_Sprimme(
-         "L", "U", nH, nX, 1.0, H, ldH, X, ldX, 0.0, rwork, nH));
+   if (isherm) {
+      CHKERR(Num_hemm_Sprimme(
+            "L", "U", nH, nX, 1.0, H, ldH, X, ldX, 0.0, rwork, nH));
+   } else {
+      CHKERR(Num_gemm_Sprimme(
+            "N", "N", nH, nX, nH, 1.0, H, ldH, X, ldX, 0.0, rwork, nH, ctx));
+   }
+
+   /* R = X' * rwork */
+
    Num_zero_matrix_Sprimme(R, nX, nX, ldR, ctx);
    CHKERR(Num_gemm_Sprimme(
          "C", "N", nX, nX, nH, 1.0, X, ldX, rwork, nH, 0.0, R, ldR, ctx));

commit bf04ff5ab4de45b3ee5407bec2e990b47beba098

Fixed issue introduced in commit 8bf6e8f69.

src/eigs/correction.c
@@ -339,10 +339,17 @@ int solve_correction_Sprimme(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
           Olsen_preconditioner_block(r, ldW, x, ldV, Bx, ldBV, blockSize, ctx);
       }
       else {
+         // In general the Olsen's approximation is not useful without
+         // preconditioning. However expanding the basis with r - approxOlsen*x
+         // helps in checking the practical convergence. Check main_iter for
+         // more details.
+
          if (primme->correctionParams.projectors.RightX &&
                ((primme->correctionParams.precondition &&
                       primme->applyPreconditioner) ||
-                     Bx != x)) {
+                     Bx != x ||
+                     (primme->locking &&
+                           primme->orth == primme_orth_implicit_I))) {
 #ifdef USE_HERMITIAN
             /*Compute a cheap approximation to OLSENS, where (x'Kinvr)/xKinvx */
             /*is approximated by e: Kinvr-e*KinvBx=Kinv(r-e*x)=Kinv(I-ct*x*x')r*/

src/eigs/main_iter.c
@@ -710,21 +710,42 @@ int main_iter_Sprimme(HEVAL *evals, SCALAR *evecs, PRIMME_INT ldevecs,
                break;
             }
 
-            /* If locking with GD and no preconditioning is running, check    */
-            /* practically convergence here. A pair in the block is marked as */
-            /* practically converged if this is sqrt(|r|^2 - |Rlocked|^2)     */
-            /* small enough to pass the convergence criterion.                */
+            // If locking with GD and no preconditioning is running, check
+            // practically convergence here. A pair in the block is marked as
+            // practically converged if the next expression is small enough to
+            // pass the convergence criterion:
+            //    sqrt(|r|^2 - |Rlocked|^2*(1 + |Xx|^2)),
+            // where Rlocked is V_locked'*r and Xx is V_locked'*x.
+            // The term Xx is not totally clear. It appears in the perturbation
+            // analysis of the eigenproblem with V_locked and x. And it is
+            // necessary to pass the next test for complex:
+            // tests/testi-100-LOBPCG_OrthoBasis_Window-100-primme_closest_abs-primme_proj_refined.F
 
             if (primme->locking && !primme->massMatrixMatvec &&
                   !primme->correctionParams.precondition &&
                   primme->correctionParams.maxInnerIterations == 0) {
                for (i = 0; i < blockSize0 && numConverged < primme->numEvals;
                      i++) {
+                  HREAL normXx = 0.0;
+                  if (primme->orth == primme_orth_explicit_I) {
+                     HSCALAR *Xx = NULL;
+                     CHKERR(Num_malloc_SHprimme(numLocked, &Xx, ctx));
+                     CHKERR(Num_zero_matrix_SHprimme(
+                           Xx, numLocked, 1, numLocked, ctx));
+                     CHKERR(Num_gemv_SHprimme("N", numLocked, basisSize, 1.0,
+                           &VtBV[ldVtBV * numLocked], ldVtBV,
+                           &hVecs[iev[i] * basisSize], 1, 0.0, Xx, 1, ctx));
+                     normXx =
+                           ABS(Num_dot_SHprimme(numLocked, Xx, 1, Xx, 1, ctx));
+                     CHKERR(Num_free_SHprimme(Xx, ctx));
+                  }
                   HREAL normRlockedi =
                         ABS(Num_dot_SHprimme(ldRlocked, &Rlocked[ldRlocked * i],
                               1, &Rlocked[ldRlocked * i], 1, ctx));
-                  HREAL newBlockNorm = sqrt(
-                        max(blockNorms[i] * blockNorms[i] - normRlockedi, 0.0));
+                  HREAL newBlockNorm =
+                        sqrt(max(blockNorms[i] * blockNorms[i] -
+                                       normRlockedi * (1. + normXx),
+                              0.0));
                   CHKERR(check_convergence_Sprimme(&V[(basisSize + i) * ldV],
                         ldV, 1 /* given X */, NULL, 0, 0 /* not given R */,
                         evecs, numLocked, ldevecs, Bevecs, ldBevecs, VtBV,