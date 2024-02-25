bf068f4ec12e6a39249e01ee940ddb5488a88419
Jeremy Myers <jermyer@sandia.gov>
2024-02-25 10:33:14 -0800

    Fix PRIMME bugs to work with custom convTestFun

--- src/eigs/main_iter.c
+++ src/eigs/main_iter.c
@@ -1536,11 +1536,6 @@ STATIC int prepare_candidates(SCALAR *V, PRIMME_INT ldV, SCALAR *W,
    *recentlyConverged = 0;
    while (1) {
       /* Recompute flags in iev(*blockSize:*blockSize+blockNormsize) */
-      /* for (i = *blockSize; i < blockNormsSize; i++) { */
-      /*    flagsBlock[i-*blockSize] = flags[iev[i]]; */
-      /*    global_idx[i - *blockSize] = numLocked + iev[i]; */
-      /* } */
-      /* Recompute flags in iev(*blockSize:*blockSize+blockNormsize) */
       for (i = *blockSize; i < *blockSize+blockNormsSize; i++) {
          flagsBlock[i - *blockSize] = flags[iev[i]];
          global_idx[i - *blockSize] = numLocked + iev[i];
